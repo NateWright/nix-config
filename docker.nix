7bf6f38a663c53479c648dd2a09fc26103d9c885
NateWright <nathanwrightbusiness@gmail.com>
2023-03-03 16:03:22 -0500

    added podman

--- docker.nix
+++ docker.nix
@@ -4,10 +4,26 @@
   # make the docker command usable to users
   environment.systemPackages = with pkgs; [ 
 	docker-compose
+  podman-compose
+
  ];
 
   # enable the docker service
-  virtualisation.docker.enable = true;
+  virtualisation = {
+    docker.enable = true;
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      # dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.dnsname.enable = true;
+    };
+  };
+  # virtualisation.docker.daemon.settings = {
+  #   "userland-proxy" = false;
+  # };
 
   users.users.nwright.extraGroups = [ "docker" ];
 }
