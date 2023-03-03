c08b2fc8b7f0cf739fc30d5b9e5ac1f1622b8933
NateWright <nathanwrightbusiness@gmail.com>
2023-03-03 16:02:21 -0500

    changed nginx to caddy

---
+++ caddy.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+{
+  services.caddy = {
+    enable = true;
+    user = "root";
+    group = "root";
+    extraConfig = ''
+      nixos.raptor-roach.ts.net {
+        redir /.well-known/carddav /cloud/remote.php/dav 301
+        redir /.well-known/caldav /cloud/remote.php/dav 301
+        handle_path /cloud*  {
+          encode gzip
+          reverse_proxy localhost:8009
+        }
+      }
+      https://nixos.raptor-roach.ts.net:444 {
+        reverse_proxy localhost:9980
+      }
+      https://nixos.raptor-roach.ts.net:446 {
+        encode gzip
+        reverse_proxy 127.0.0.1:2343
+      }
+    '';
+  };
+}
