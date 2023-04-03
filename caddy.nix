13a6ae2860d58dbf17aac367bf884b92962bbd88
NateWright <nathanwrightbusiness@gmail.com>
2023-04-03 11:30:44 -0400

    added well known

--- caddy.nix
+++ caddy.nix
@@ -6,15 +6,16 @@
     group = "root";
     extraConfig = ''
       nixos.raptor-roach.ts.net {
-        redir /.well-known/carddav /cloud/remote.php/dav 301
-        redir /.well-known/caldav /cloud/remote.php/dav 301
+        redir /.well-known/carddav /cloud/remote.php/carddav 301
+	      redir /.well-known/caldav /cloud/remote.php/caldav 301
         handle_path /cloud*  {
           encode gzip
           reverse_proxy localhost:8009
         }
       }
       https://nixos.raptor-roach.ts.net:444 {
-        reverse_proxy localhost:9980
+        encode gzip
+        reverse_proxy 127.0.0.1:9980
       }
       https://nixos.raptor-roach.ts.net:446 {
         encode gzip
