993a7707a2ac2da6895a93fde522462ca3794be2
nwright <nathanwrightbusiness@gmail.com>
2023-08-22 20:35:14 -0400

    added comment for hyprland

--- devices/surface/configuration.nix
+++ devices/surface/configuration.nix
@@ -52,6 +52,7 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  # programs.hyprland.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
@@ -122,6 +123,7 @@
     nixpkgs-fmt
     tailscale
     terminator
+    kitty
     libcamera
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
