832295f37db8c5c8dd210715efdf63f89dfa9259
nwright <nathanwrightbusiness@gmail.com>
2023-08-28 16:18:39 -0400

    added gbar

--- devices/surface/configuration.nix
+++ devices/surface/configuration.nix
@@ -128,6 +128,7 @@
     terminator
     zip
     unzip
+    gBar.defaultPackage.x86_64-linux
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
