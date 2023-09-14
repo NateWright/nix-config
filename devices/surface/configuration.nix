49a4e182ee964b15aa507f0cea76329ae15528b8
nwright <nathanwrightbusiness@gmail.com>
2023-09-14 13:17:10 -0400

    update

--- devices/surface/configuration.nix
+++ devices/surface/configuration.nix
@@ -146,6 +146,8 @@ in
     unzip
     pika-backup
     tokyo-night-sddm
+    nextcloud-client
+    tailscale-systray
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
