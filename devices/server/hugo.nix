d2ee1506b5874eefef197a8695396541599181bf
NateWright <nathanwrightbusiness@gmail.com>
2023-10-05 18:47:00 -0400

    updated rev

--- devices/server/hugo.nix
+++ devices/server/hugo.nix
@@ -5,6 +5,7 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "NateWright";
     repo = "NateWright";
+    rev = "8ba2ca59df74f72d310a7ee6100b76177c5026cb";
   };
 
   nativeBuildInputs = [ hugo ];
