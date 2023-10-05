f2066044abd4dc069f493d30cf9ca8792e845d2c
NateWright <nathanwrightbusiness@gmail.com>
2023-10-05 18:47:59 -0400

    updated sha256

--- devices/server/hugo.nix
+++ devices/server/hugo.nix
@@ -6,6 +6,7 @@ stdenv.mkDerivation {
     owner = "NateWright";
     repo = "NateWright";
     rev = "8ba2ca59df74f72d310a7ee6100b76177c5026cb";
+    sha256 = "sha256-4/pKZ6fRV4DcY3vdKpxoT3ONGFTomA3aiL7h1W7Xfh8=";
   };
 
   nativeBuildInputs = [ hugo ];
