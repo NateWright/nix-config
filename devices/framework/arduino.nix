906f959b40d7e5b6b527f96b632e0515a0c8e8e3
nwright <nathanwrightbusiness@gmail.com>
2024-01-23 15:14:30 -0500

    Added support for ros camera

--- devices/framework/arduino.nix
+++ devices/framework/arduino.nix
@@ -4,5 +4,6 @@
     SUBSYSTEM=="tty", ATTRS{idVendor}=="2a03", ATTRS{idProduct}=="0043", MODE="666", SYMLINK+="stretch/handle", GROUP="dialout"
     SUBSYSTEMS=="usb", ATTRS{idVendor}=="239a", GROUP="plugdev", MODE="0666"
     SUBSYSTEM == "tty", KERNEL=="ttyACM[0-9]*", MODE="0660"
+    SUBSYSTEM == "usb", ATTRS{idVendor}=="03e7", MODE="0666"
   '';
 }
