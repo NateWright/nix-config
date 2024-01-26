e7362e23d1979fba93f7f1b508edf2c1b5380562
NateWright <nathanwrightbusiness@gmail.com>
2024-01-26 12:06:44 -0500

    added packages

--- devices/server/configuration.nix
+++ devices/server/configuration.nix
@@ -160,6 +160,7 @@
     git
     distrobox
     bat
+    gnumake
     # timeshift
   ];
 
@@ -197,7 +198,7 @@
     enable = true;
 
     # always allow traffic from your Tailscale network
-    trustedInterfaces = [ "tailscale0" "docker0" "br-collabora" "br-photprism" ];
+    trustedInterfaces = [ "tailscale0" "docker0" "br-collabora" "br-photprism" "br-onlyoffice" ];
 
     # allow the Tailscale UDP port through the firewall
     allowedUDPPorts = [ config.services.tailscale.port ];
