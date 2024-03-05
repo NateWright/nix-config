c36942e277e5d031918c40d40a363a877b2c3cb6
nwright <nathanwrightbusiness@gmail.com>
2024-03-04 20:13:10 -0500

    init plasma

--- common/de/plasma.nix
+++ common/de/plasma.nix
@@ -2,14 +2,25 @@
 {
   services.xserver = {
     enable = true;
-    layout = "us";
-    xkbVariant = "";
+    xkb =
+      {
+        layout = "us";
+        variant = "";
+      };
 
     displayManager.sddm.enable = true;
+    displayManager.sddm.wayland.enable = true;
     # displayManager.sddm.theme = "tokyo-night-sddm";
 
     desktopManager.plasma6.enable = true;
 
   };
+
+  programs.dconf.enable = true;
+  services.packagekit.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    kdePackages.discover
+  ];
 
 }
