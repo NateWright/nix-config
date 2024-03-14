b7d1e31036b53079a5138c957f0e745f76f46177
nwright <nathanwrightbusiness@gmail.com>
2024-03-14 18:58:17 -0400

    xone working

--- devices/desktop/xone/default.nix
+++ devices/desktop/xone/default.nix
@@ -7,18 +7,9 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "medusalix";
     repo = pname;
-    rev = "refs/tags/v${version}";
-    sha256 = "948d2302acdd6333295eaba4da06d96677290ad3";
+    rev = "948d2302acdd6333295eaba4da06d96677290ad3";
+    sha256 = "sha256-srAEw1ai5KT0rmVUL3Dut9R2mNb00AAZVCcINikh2sM=";
   };
-
-  patches = [
-    # Fix build on kernel 6.3
-    (fetchpatch {
-      name = "kernel-6.3.patch";
-      url = "https://github.com/medusalix/xone/commit/bbf0dcc484c3f5611f4e375da43e0e0ef08f3d18.patch";
-      hash = "sha256-A2OzRRk4XT++rS6k6EIyiPy/LJptvVRUxoP7CIGrPWU=";
-    })
-  ];
 
   setSourceRoot = ''
     export sourceRoot=$(pwd)/${src.name}
