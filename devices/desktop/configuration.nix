724dc8838b0b781b5f4f336fc4f11785378a475a
nwright <nathanwrightbusiness@gmail.com>
2024-03-20 20:33:45 -0400

    added gc

--- devices/desktop/configuration.nix
+++ devices/desktop/configuration.nix
@@ -29,10 +29,13 @@
     };
   };
 
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
+  nix = {
+    settings.auto-optimise-store = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 10d";
+    };
   };
 
 
