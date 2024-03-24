06e98020a576eedccac6801909fbb7fafa2fd45a
nwright <nathanwrightbusiness@gmail.com>
2024-03-24 11:25:01 -0400

    fix for fprintd

--- devices/framework/configuration.nix
+++ devices/framework/configuration.nix
@@ -24,6 +24,7 @@ in
     overlays = [
       # Add overlays your own flake exports (from overlays and pkgs dir):
       outputs.overlays.unstable-packages
+      outputs.overlays.modifications
     ];
     # Configure your nixpkgs instance
     config = {
