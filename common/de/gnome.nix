2bcad7480d1491f935aae1e6887233c9f4000ba8
nwright <nathanwrightbusiness@gmail.com>
2024-03-26 16:41:10 -0400

    added pop shell

--- common/de/gnome.nix
+++ common/de/gnome.nix
@@ -17,6 +17,7 @@
     gnome-extension-manager
     gnome.nautilus-python
     gnome.sushi
+    gnomeExtensions.pop-shell
     nautilus-open-any-terminal
   ];
   services.xserver.desktopManager.gnome.extraGSettingsOverridePackages = with pkgs; [
