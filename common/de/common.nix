ddd900bd7f1e457a11ac4f01b7483a6bef642ca2
nwright <nathanwrightbusiness@gmail.com>
2024-04-27 14:47:04 -0400

    formatting

--- common/de/common.nix
+++ common/de/common.nix
@@ -1,12 +1,10 @@
-{ config, lib, pkgs, ... }:
-{
+{ config, lib, pkgs, ... }: {
   services.xserver = {
     enable = true;
-    xkb =
-      {
-        layout = "us";
-        variant = "";
-      };
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
   };
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
