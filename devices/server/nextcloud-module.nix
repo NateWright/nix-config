3b98c665c50edcdb32df391617fa92980db456a9
nwright <nathanwrightbusiness@gmail.com>
2024-05-09 16:33:57 -0400

    test fastcgi on remote.php

--- devices/server/nextcloud-module.nix
+++ devices/server/nextcloud-module.nix
@@ -1238,6 +1238,29 @@ in {
               return 404;
             '';
           };
+          "~ remote.php(?:$|/)" = {
+            priority = 500;
+            extraConfig = ''
+              # legacy support (i.e. static files and directories in cfg.package)
+              rewrite ^/(?!index|remote|public|cron|core\/ajax\/update|status|ocs\/v[12]|updater\/.+|oc[s${
+                optionalString (!ocmProviderIsNotAStaticDirAnymore) "m"
+              }]-provider\/.+|.+\/richdocumentscode\/proxy) /index.php$request_uri;
+              include ${config.services.nginx.package}/conf/fastcgi.conf;
+              fastcgi_split_path_info ^(.+?\.php)(\\/.*)$;
+              set $path_info $fastcgi_path_info;
+              try_files $fastcgi_script_name =404;
+              fastcgi_param PATH_INFO $path_info;
+              fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+              fastcgi_param HTTPS ${if cfg.https then "on" else "off"};
+              fastcgi_param modHeadersAvailable true;
+              fastcgi_param front_controller_active true;
+              fastcgi_pass unix:${fpm.socket};
+              fastcgi_intercept_errors on;
+              fastcgi_request_buffering on;
+              chunked_transfer_encoding on;
+              fastcgi_read_timeout ${builtins.toString cfg.fastcgiTimeout}s;
+            '';
+          };
           "~ \\.php(?:$|/)" = {
             priority = 500;
             extraConfig = ''
@@ -1256,8 +1279,6 @@ in {
               fastcgi_param front_controller_active true;
               fastcgi_pass unix:${fpm.socket};
               fastcgi_intercept_errors on;
-              fastcgi_request_buffering ${cfg.nginx.enableFastcgiRequestBuffering};
-              chunked_transfer_encoding on;
               fastcgi_read_timeout ${builtins.toString cfg.fastcgiTimeout}s;
             '';
           };
