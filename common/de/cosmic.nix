8c856d3ad1d3135e079da401310dba97e1dbc2e3
nwright <nathanwrightbusiness@gmail.com>
2024-07-13 11:00:29 -0400

    keyring

--- common/de/cosmic.nix
+++ common/de/cosmic.nix
@@ -1,8 +1,9 @@
-{ config, lib, pkgs, ... }:
-{
+{ config, lib, pkgs, ... }: {
   services = {
     desktopManager.cosmic.enable = true;
     displayManager.cosmic-greeter.enable = true;
+    gnome.gnome-keyring.enable = true;
   };
+  security.pam.services.cosmic-greeter.enableGnomeKeyring = true;
 
 }
