7cf1e90ae52fa343c888f7900baa14f26c5f9937
nwright <nathanwrightbusiness@gmail.com>
2024-07-15 21:37:47 -0400

    surface: removed

--- devices/surface/arduino.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, ... }:
-{
-  services.udev.extraRules = ''
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2a03", GROUP="plugdev", MODE="0666", SYMLINK+="stretch-handle"
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="239a", GROUP="plugdev", MODE="0666"
-  '';
-}
--- devices/surface/cinnamon.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, inputs, ... }:
-{
-  services.xserver.desktopManager.cinnamon.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  environment.systemPackages = [
-    pkgs.gnome.gnome-software
-  ];
-}
