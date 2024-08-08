388bb6064398112514c0aa61ac5d4748b2db4c6d
NateWright <nathanwrightbusiness@gmail.com>
2024-08-07 22:02:53 -0400

    server-nixos-1: add prune to docker

--- devices/server-nixos-1/docker/default.nix
+++ devices/server-nixos-1/docker/default.nix
@@ -1,7 +1,12 @@
 { ... }: {
 
   # enable the docker service
-  virtualisation = { docker.enable = true; };
+  virtualisation = {
+    docker = {
+      enable = true;
+      autoPrune.enable = true;
+    };
+  };
 
   virtualisation.oci-containers = { backend = "docker"; };
 }
