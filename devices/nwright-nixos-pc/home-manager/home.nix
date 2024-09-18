6fe99a0c18eb3d807f039ae2fce144ed6dbb086e
nwright <nathanwrightbusiness@gmail.com>
2024-09-18 19:53:25 -0400

    nwright-nixos-pc: fixes

--- devices/nwright-nixos-pc/home-manager/home.nix
+++ devices/nwright-nixos-pc/home-manager/home.nix
@@ -1,7 +1,13 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 
-{ lib, config, pkgs, ... }: {
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
   # You can import other home-manager modules here
   imports = [
     # If you want to use home-manager modules from other flakes (such as nix-colors):
@@ -57,7 +63,7 @@
   programs.zellij.enable = true;
 
   # Nicely reload system units when changing configs
-  systemd.user.startServices = "sd-switch";
+  # systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
