28b477feae148346742feddd2475308f2946017d
nwright <nathanwrightbusiness@gmail.com>
2024-11-17 19:14:55 -0500

    vscode: catppuccin overhall

--- common/home-manager/vscode.nix
+++ common/home-manager/vscode.nix
@@ -1,26 +1,36 @@
-{ lib, config, pkgs, ... }: {
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-      pkief.material-icon-theme
-      pkief.material-product-icons
-      ms-vscode.cpptools-extension-pack
-      ms-vscode.cpptools
-      ms-vsliveshare.vsliveshare
-      # github.copilot
-      streetsidesoftware.code-spell-checker
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "poptheme";
-        publisher = "ArtisanByteCrafter";
-        version = "1.0.4";
-        sha256 = "sha256-GXLG4ojoBTvx6EpskEA3UhXXLKrCAGeRhjMgcl5pRog=";
-      }
-    ];
+    extensions =
+      with pkgs.vscode-extensions;
+      [
+        jnoortheen.nix-ide
+        pkief.material-icon-theme
+        pkief.material-product-icons
+        catppuccin.catppuccin-vsc
+        ms-vscode.cpptools-extension-pack
+        ms-vscode.cpptools
+        # ms-vsliveshare.vsliveshare
+        # github.copilot
+        streetsidesoftware.code-spell-checker
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "poptheme";
+          publisher = "ArtisanByteCrafter";
+          version = "1.0.4";
+          sha256 = "sha256-GXLG4ojoBTvx6EpskEA3UhXXLKrCAGeRhjMgcl5pRog=";
+        }
+      ];
     userSettings = {
       "editor.formatOnSave" = true;
-      "workbench.colorTheme" = "Pop Dark";
+      "workbench.colorTheme" = "Catppuccin Macchiato";
       "workbench.productIconTheme" = "material-product-icons";
       "workbench.iconTheme" = "material-icon-theme";
       "C_Cpp.clang_format_fallbackStyle" = "{ BasedOnStyle: Google, IndentWidth: 4, TabWidth: 4, ColumnLimit: 0}";
