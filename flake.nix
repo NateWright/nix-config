735050528c2275fb3a32f7bf3f29a730c7971e6c
nwright <nathanwrightbusiness@gmail.com>
2024-11-17 19:15:21 -0500

    formatting

--- flake.nix
+++ flake.nix
@@ -3,8 +3,12 @@
   description = "flake for nwright's machines";
 
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-24.05"; };
-    nixpkgs-unstable = { url = "github:Nixos/nixpkgs/nixos-unstable"; };
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-24.05";
+    };
+    nixpkgs-unstable = {
+      url = "github:Nixos/nixpkgs/nixos-unstable";
+    };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager = {
@@ -25,8 +29,18 @@
     catppuccin.url = "github:catppuccin/nix";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-hardware, nixos-cosmic
-    , home-manager, home-manager-unstable, vscode-server, catppuccin, ...
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      nixos-hardware,
+      nixos-cosmic,
+      home-manager,
+      home-manager-unstable,
+      vscode-server,
+      catppuccin,
+      ...
     }@inputs:
     let
       inherit (self) outputs;
@@ -38,15 +52,17 @@
         "x86_64-darwin"
       ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
-    in rec {
+    in
+    rec {
       overlays = import ./overlays { inherit inputs; };
-      packages =
-        forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+      packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
 
       nixosConfigurations = {
         nwright-framework = nixpkgs-unstable.lib.nixosSystem {
           system = "x86_64-linux";
-          specialArgs = { inherit outputs inputs; };
+          specialArgs = {
+            inherit outputs inputs;
+          };
           modules = [
             nixos-cosmic.nixosModules.default
             nixos-hardware.nixosModules.framework-13-7040-amd
@@ -56,7 +72,9 @@
         };
         nwright-nixos-pc = nixpkgs-unstable.lib.nixosSystem {
           system = "x86_64-linux";
-          specialArgs = { inherit outputs inputs; };
+          specialArgs = {
+            inherit outputs inputs;
+          };
           modules = [
             nixos-cosmic.nixosModules.default
             catppuccin.nixosModules.catppuccin
@@ -65,7 +83,9 @@
         };
         server-nixos-1 = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          specialArgs = { inherit outputs inputs; };
+          specialArgs = {
+            inherit outputs inputs;
+          };
           modules = [
             vscode-server.nixosModules.default
             ./devices/server-nixos-1/configuration.nix
@@ -73,40 +93,37 @@
         };
         linode-nixos-1 = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          specialArgs = { inherit outputs inputs; };
+          specialArgs = {
+            inherit outputs inputs;
+          };
           modules = [ ./devices/linode-nixos-1/configuration.nix ];
         };
       };
       homeConfigurations = {
-        "nwright@nwright-nixos-pc" =
-          home-manager-unstable.lib.homeManagerConfiguration {
-            pkgs =
-              nixpkgs-unstable.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-            # extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-            # > Our main home-manager configuration file <
-            modules = [
-              catppuccin.homeManagerModules.catppuccin
-              ./devices/nwright-nixos-pc/home-manager/home.nix
-            ];
-          };
+        "nwright@nwright-nixos-pc" = home-manager-unstable.lib.homeManagerConfiguration {
+          pkgs = nixpkgs-unstable.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          # extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+          # > Our main home-manager configuration file <
+          modules = [
+            catppuccin.homeManagerModules.catppuccin
+            ./devices/nwright-nixos-pc/home-manager/home.nix
+          ];
+        };
 
-        "nwright@nwright-framework" =
-          home-manager-unstable.lib.homeManagerConfiguration {
-            pkgs =
-              nixpkgs-unstable.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-            extraSpecialArgs = {
-              inherit outputs inputs;
-            }; # Pass flake inputs to our config
-            # > Our main home-manager configuration file <
-            modules = [
-              catppuccin.homeManagerModules.catppuccin
-              ./devices/nwright-framework/home-manager/home.nix
-            ];
-          };
+        "nwright@nwright-framework" = home-manager-unstable.lib.homeManagerConfiguration {
+          pkgs = nixpkgs-unstable.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = {
+            inherit outputs inputs;
+          }; # Pass flake inputs to our config
+          # > Our main home-manager configuration file <
+          modules = [
+            catppuccin.homeManagerModules.catppuccin
+            ./devices/nwright-framework/home-manager/home.nix
+          ];
+        };
 
         "nwright@server-nixos-1" = home-manager.lib.homeManagerConfiguration {
-          pkgs =
-            nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
           extraSpecialArgs = {
             inherit outputs inputs;
           }; # Pass flake inputs to our config
@@ -118,4 +135,3 @@
 
     };
 }
-
