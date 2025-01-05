9d8bb4af41676d20e544d39ed3ddf2f3ba3bf82e
nwright <nathanwrightbusiness@gmail.com>
2025-01-05 13:02:48 -0500

    gtk-plasma: cleanup

--- common/home-manager/gtk-plasma.nix
+++ common/home-manager/gtk-plasma.nix
@@ -12,29 +12,6 @@
       name = "Breeze-Dark";
       package = pkgs.kdePackages.breeze-gtk;
     };
-
-    # gtk2.extraConfig = ''
-    #   gtk-enable-animations=1
-    #   gtk-primary-button-warps-slider=1
-    #   gtk-toolbar-style=3
-    #   gtk-menu-images=1
-    #   gtk-button-images=1
-    #   gtk-cursor-theme-size=24
-    #   gtk-sound-theme-name="ocean"
-    #   gtk-icon-theme-name="breeze-dark"
-    #   gtk-font-name="Noto Sans,  10"
-    # '';
-
-    # gtk3.extraConfig = {
-    #   gtk-cursor-theme-size = 24;
-    #   gtk-application-prefer-dark-theme = 0;
-    # };
-
-    # gtk4.extraConfig = {
-    #   gtk-cursor-theme-size = 24;
-    #   gtk-application-prefer-dark-theme = 0;
-    # };
-
   };
 
   dconf.settings = {
