9939eaddac92baa89186c8868215b3628b4f3b9b
nwright <nathanwrightbusiness@gmail.com>
2025-01-06 20:56:39 -0500

    nwright-framework: refactor and cleanup

--- devices/nwright-framework/configuration.nix
+++ devices/nwright-framework/configuration.nix
@@ -10,8 +10,11 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./fonts.nix
+
+    ../../common/nix-settings.nix
     ../../common/pkgs.nix
     ../../common/pkgs-cli.nix
+
     ../../common/de/common.nix
     ../../common/de/cosmic.nix
   ];
@@ -30,28 +33,9 @@
       allowUnfree = true;
     };
   };
-
-  nix.settings = {
-    # Enable flakes and new 'nix' command
-    experimental-features = "nix-command flakes";
-    # Deduplicate and optimize nix store
-    auto-optimise-store = true;
-    trusted-users = [ "nwright" ];
-
-    substituters = [
-      "https://hyprland.cachix.org"
-      "https://cosmic.cachix.org/"
-    ];
-    trusted-public-keys = [
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-    ];
-  };
   # Bootloader.
   boot = {
-
     loader = {
-
       systemd-boot = {
         enable = true;
         configurationLimit = 5;
@@ -59,7 +43,6 @@
 
       efi.canTouchEfiVariables = true;
       timeout = 0;
-
     };
     kernelPackages = pkgs.linuxPackages_xanmod_latest;
   };
@@ -78,69 +61,98 @@
       "noatime"
     ];
     "/swap".options = [ "noatime" ];
-
   };
 
   swapDevices = [ { device = "/swap/swapfile"; } ];
 
-  networking.hostName = "nwright-framework"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking = {
+    hostName = "nwright-framework";
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networkmanager.enable = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    firewall = {
+      # enable the firewall
+      enable = true;
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-  systemd.services.NetworkManager-wait-online.enable = false;
+      # always allow traffic from your Tailscale network
+      trustedInterfaces = [ "tailscale0" ];
+
+      # allow the Tailscale UDP port through the firewall
+      allowedUDPPorts = [
+        config.services.tailscale.port
+        7236
+        5353
+      ];
+
+      # allow you to SSH in over the public internet
+      allowedTCPPorts = [
+        22
+        7236
+        7250
+      ];
+    };
+  };
+
   # Set your time zone.
   time.timeZone = "America/New_York";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  # services.printing.drivers = [ pkgs.hplip ];
-  services.avahi.enable = true;
-  services.avahi.nssmdns4 = true;
-  # for a WiFi printer
-  services.avahi.openFirewall = true;
+  services = {
+    # Printing
+    printing.enable = true;
+    # printing.drivers = [ pkgs.hplip ];
+    avahi.enable = true;
+    avahi.nssmdns4 = true;
+    avahi.openFirewall = true;
 
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+    };
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    fwupd = {
+      enable = true;
+      extraRemotes = [ "lvfs-testing" ];
+    };
+
+    flatpak.enable = true;
+    tailscale.enable = true;
   };
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.settings = {
-    General = {
-      Experimental = true;
-      ControllerMode = "dual";
-      MultiProfile = "multiple";
-      AutoEnable = true;
+  virtualisation = {
+    docker.enable = true;
+    docker.storageDriver = "btrfs";
+    libvirtd.enable = true;
+  };
+
+  security.rtkit.enable = true; # Pipewire enhancement
+
+  hardware = {
+    bluetooth = {
+      enable = true;
+      settings = {
+        General = {
+          Experimental = true;
+          ControllerMode = "dual";
+          MultiProfile = "multiple";
+          AutoEnable = true;
+        };
+      };
     };
   };
 
@@ -209,70 +221,6 @@
     };
     zsh.enable = true;
   };
-  services.fwupd = {
-    enable = true;
-    extraRemotes = [ "lvfs-testing" ];
-  };
-
-  services.flatpak.enable = true;
-  # virtualisation.podman.enable = true;
-  virtualisation.docker.enable = true;
-  virtualisation.docker.storageDriver = "btrfs";
-  virtualisation.libvirtd.enable = true;
-
-  # hardware.xone.enable = true;
-  # hardware.xpadneo.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # services.fprintd = {
-  #   enable = true;
-  #   # package = pkgs.fprintd-tod;
-  #   # tod = {
-  #   #   enable = true;
-  #   #   driver = pkgs.libfprint-2-tod1-goodix;
-  #   # };
-  # };
-
-  services.tailscale.enable = true;
-  networking.firewall = {
-    # enable the firewall
-    enable = true;
-
-    # always allow traffic from your Tailscale network
-    trustedInterfaces = [ "tailscale0" ];
-
-    # allow the Tailscale UDP port through the firewall
-    allowedUDPPorts = [
-      config.services.tailscale.port
-      7236
-      5353
-    ];
-
-    # allow you to SSH in over the public internet
-    allowedTCPPorts = [
-      22
-      7236
-      7250
-    ];
-  };
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -281,5 +229,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
-
 }
--- devices/nwright-framework/home-manager/home.nix
+++ devices/nwright-framework/home-manager/home.nix
@@ -2,9 +2,7 @@
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 
 {
-  pkgs,
   inputs,
-  outputs,
   ...
 }:
 {
@@ -19,35 +17,10 @@
     inputs.catppuccin.homeManagerModules.catppuccin
   ];
 
-  # nixpkgs = {
-  #   # You can add overlays here
-  #   overlays = [
-  #     # If you want to use overlays exported from other flakes:
-  #     # neovim-nightly-overlay.overlays.default
-
-  #     # Or define it inline, for example:
-  #     # (final: prev: {
-  #     #   hi = final.hello.overrideAttrs (oldAttrs: {
-  #     #     patches = [ ./change-hello-to-hi.patch ];
-  #     #   });
-  #     # })
-  #     outputs.overlays.additions
-  #   ];
-  #   # Configure your nixpkgs instance
-  #   config = {
-  #     # Disable if you don't want unfree packages
-  #     allowUnfree = true;
-  #     # Workaround for https://github.com/nix-community/home-manager/issues/2942
-  #     allowUnfreePredicate = (_: true);
-  #   };
-  # };
-
   home = {
     username = "nwright";
     homeDirectory = "/home/nwright";
   };
-
-  news.display = "silent";
 
   catppuccin = {
     enable = true;
@@ -64,15 +37,7 @@
     zellij.enable = true;
   };
 
-  # Enable bluetooth pause/play
-  services.mpris-proxy.enable = true;
-
-  # Gnome fractional scaling
-  # dconf.settings = {
-  #   "org/gnome/mutter" = {
-  #     experimental-features = [ "scale-monitor-framebuffer" ];
-  #   };
-  # };
+  services.mpris-proxy.enable = true; # Enable bluetooth pause/play
 
   # Nicely reload system units when changing configs
   # systemd.user.startServices = "sd-switch";
