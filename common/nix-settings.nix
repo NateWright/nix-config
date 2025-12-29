734a43b971f58fb3f58a02f3812c8f42359059d4
NateWright <nathanwrightbusiness@gmail.com>
2025-12-28 22:18:27 -0500

    nix-settings: using nix helper to clean

--- common/nix-settings.nix
+++ common/nix-settings.nix
@@ -16,15 +16,21 @@
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
       ];
     };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 10d";
-    };
-    optimise = {
-      automatic = true;
-      dates = "03:45";
-    };
+    # gc = {
+    #   automatic = true;
+    #   dates = "weekly";
+    #   options = "--delete-older-than 10d";
+    # };
+    # optimise = {
+    #   automatic = true;
+    #   dates = "03:45";
+    # };
+  };
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep 3 --optimise";
+    clean.dates = "weekly";
   };
 
 }
