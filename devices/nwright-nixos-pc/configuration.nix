bbcc4de7e94ecd6b4f438a604a6da627d23bdf1a
Nathan Wright <nathanwrightbusiness@gmail.com>
2026-06-25 19:19:38 -0400

    nwright-nixos-pc/configuration: user standard kernel

--- devices/nwright-nixos-pc/configuration.nix
+++ devices/nwright-nixos-pc/configuration.nix
@@ -33,7 +33,7 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackages_latest;
+    # kernelPackages = pkgs.linuxPackages_latest;
     supportedFilesystems = [ "ntfs" ];
     #   blacklistedKernelModules = [
     #     "xpad"
